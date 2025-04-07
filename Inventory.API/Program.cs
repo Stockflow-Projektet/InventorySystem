2abc6ec0974e1b8a9e7b6f81a65770f21be74f46
MJR-Git <mayarexen@protonmail.com>
2025-04-07 12:23:55 +0200

    errorfree backend

--- Inventory.API/Program.cs
+++ Inventory.API/Program.cs
@@ -29,13 +29,6 @@ builder.Services.AddScoped<IOrderFactory>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
