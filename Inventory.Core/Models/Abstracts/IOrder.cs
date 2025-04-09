490b18b44e922d04934ad70bbf9b621ded070c6d
MJR-Git <mayarexen@protonmail.com>
2025-04-09 10:46:23 +0200

    corrected Order to have a list of OrderItems property

--- Inventory.Core/Models/Abstracts/IOrder.cs
+++ Inventory.Core/Models/Abstracts/IOrder.cs
@@ -4,5 +4,5 @@ public interface IOrder
 {
     public int OrderId { get; set; }
     public DateTime OrderDate { get; set; }
-    public int productIds { get; set; }
+    public List<IOrderItem> OrderItems { get; set; }
 }
